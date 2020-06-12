5f8f1dc5d0ebf65974f008f8f8844df1e3ec80e3
ebssbd <info@ebssbd.com>
2020-06-12 13:43:27 +0000

    Upload from server

--- Scripts/_global.js
+++ Scripts/_global.js
@@ -9,6 +9,11 @@ function btnEditClick(e) {
     createModalContainer();
     ajaxModalLoad(e.attr("myUrl") + "/" + e.val() + "/", modalId);
 }
+//btnDetailsClick
+function btnDetailsClick(e) {
+    createModalContainer();
+    ajaxModalLoad(e.attr("myUrl") + "/" + e.val() + "/", modalId);
+}
 
 function btnDeleteClick(e) {
     createModalContainer();
@@ -43,11 +48,12 @@ function ajaxModalLoad(url, modalId) {
         }
     });
 }
-
 function formValidSend() {
     var form = $('#' + modalId).find('form');
+
     $.validator.unobtrusive.parse(form);
     form.validate();
+    $("#loader_view").show();
     if (form.valid()) {
         $.ajax({
             url: form.attr('ajaxPostUrl'),
@@ -55,11 +61,50 @@ function formValidSend() {
             data: form.serialize(),
             async: false,
             success: function (data) {
+                $("#loader_view").hide();
                 $('#' + modalId).modal('hide');
                 $('.mvc-grid').mvcgrid({ reload: true });
             },
             error: function () {
                 alert('formValidSend - save data with ajax was failed !!! :(');
+            }
+        });
+    } else {
+        return false;
+    }
+}
+
+function formValidSend1() {
+    var form = $('#' + modalId).find('form');
+    $(".validation-summary-valid ul").append("");
+    $.validator.unobtrusive.parse(form);
+    form.validate();
+    $("#loader_view").show();
+    if (form.valid()) {
+        $.ajax({
+            url: form.attr('ajaxPostUrl'),
+            type: "POST",
+            data: form.serialize(),
+            async: false,
+            success: function (data) {
+                debugger;
+                if (data == "true") {
+                    $("#loader_view").hide();
+                    $(".validation-summary-valid ul").append("");
+                    $('#' + modalId).modal('hide');
+                    $('.mvc-grid').mvcgrid({ reload: true });
+                }else if (data.length > 0) {
+                    $.each(data, function (key, val) {
+                        
+                        $(".validation-summary-valid ul").append("<li>" + val.ErrorMessage+"</li>");
+                    });
+                }
+                $("#loader_view").hide();
+            },
+            error: function (data) {
+                
+                alert('formValidSend - save data with ajax was failed !!! :(');
+                $("#loader_view").hide();
             }
         });
     } else {
@@ -74,5 +119,5 @@ $(document).on('keypress', function (e) {
 });
 
 $(document).ready(function () {
-    $('.mvc-grid').mvcgrid();
+    //$('.mvc-grid').mvcgrid();
 });
